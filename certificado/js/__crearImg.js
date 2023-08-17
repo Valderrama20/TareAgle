a429e63b45d72be0fe4951a139f2a0d2147170e3
Valderrama20 <jose23122009@gmai.com>
2023-08-17 16:12:40 -0300

    detalles

--- certificado/js/__crearImg.js
+++ certificado/js/__crearImg.js
@@ -1,46 +1,39 @@
-   const btnDescargar = document.getElementById("btnDescargar");
-   const imagenDescargable = document.getElementById("imagenDescargable");
+document.addEventListener("DOMContentLoaded", () => {
+    const btnDescargar = document.getElementById("btnDescargar");
+    const imagenDescargable = document.getElementById("imagenDescargable");
 
+    const obtenerParametro = (nombre) => {
+        const queryString = window.location.search;
+        const parametros = new URLSearchParams(queryString);
+        return parametros.get(nombre);
+    };
 
-// Obtener la cadena de consulta de la URL
-var queryString = window.location.search;
-        
-// Crear un objeto URLSearchParams a partir de la cadena de consulta
-var parametros = new URLSearchParams(queryString);
-
-// Obtener los valores de los parámetros de la consulta
-var carbonFootprintLevel = parametros.get('carbonFootprintLevel');
-var carbonFootprin1 = parametros.get('carbonFootprin1');
-
-const changeImg = (valor = "Bajo") => {
     const carbonEmission = {
         Bajo: "./img/img1.jpeg",
         Moderado: "./img/img2.jpeg",
         Medio: "./img/img4.jpeg",
         Alto: "./img/img4.jpeg"
-     }
-       
-    imagenDescargable.src = carbonEmission[valor]
-}
+    };
 
+    const cambiarImagen = (valor = "Bajo") => {
+        imagenDescargable.src = carbonEmission[valor];
+    };
 
-changeImg(carbonFootprintLevel)
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    
-    btnDescargar.addEventListener("click", () => {
-        // Crear un enlace temporal
+    const descargarImagen = () => {
         const enlaceTemporal = document.createElement("a");
         enlaceTemporal.href = imagenDescargable.src;
-        enlaceTemporal.download = "Mi_huella_de_carbono.jpg"; // Nombre del archivo a descargar
+        enlaceTemporal.download = "Mi_huella_de_carbono.jpg";
 
-        // Simular un clic en el enlace
         enlaceTemporal.click();
 
-        // Liberar el enlace temporal
         URL.revokeObjectURL(enlaceTemporal.href);
-    });
+    };
+
+    const carbonFootprintLevel = obtenerParametro('carbonFootprintLevel');
+
+    cambiarImagen(carbonFootprintLevel);
+
+    btnDescargar.addEventListener("click", descargarImagen);
 });
 
  
